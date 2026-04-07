6871f27e6ba3a74627576ab1fba45c3b38f5d2ab
amelcontesa <ameliacontesa2410@gmail.com>
2026-04-07 14:29:20 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
 import streamlit as st
+from PIL import Image
+import numpy as np
 
 st.title("Aplikasi Klasifikasi Kucing vs Anjing 🐱🐶")
 
 uploaded_file = st.file_uploader("Upload gambar", type=["jpg", "png", "jpeg"])
 
 if uploaded_file is not None:
-    st.image(uploaded_file, caption="Gambar yang diupload", use_column_width=True)
+    image = Image.open(uploaded_file)
+    st.image(image, caption="Gambar yang diupload", use_column_width=True)
+    
+    # simulasi prediksi sederhana
+    img_array = np.array(image)
+    rata_rata = img_array.mean()
     
     st.write("### Hasil Prediksi:")
-    st.success("Ini contoh hasil (dummy): Kucing 🐱")
+    
+    if rata_rata > 120:
+        st.success("Prediksi: Anjing 🐶")
+    else:
+        st.success("Prediksi: Kucing 🐱")
